e41980b2d9467fde787bb1df44405ce938338e63
XiaMoHuaHuo_CN <79651409+XiaMoHuaHuo-CN@users.noreply.github.com>
2022-01-24 23:26:09 +0800

    Add files via upload

--- print.php
+++ print.php
@@ -1,4 +1,8 @@
 <?php
 $data = json_decode(file_get_contents('http://jiuli.xiaoapi.cn/i/pixiv_img.php?r18=2&key=JiuLiapi'));
-header("Location: {$data->imgurl}");
+if (isset($_GET['imgurl'])) {
+	echo $data->imgurl;
+} else {
+	header("Location: {$data->imgurl}");
+}
 ?>
